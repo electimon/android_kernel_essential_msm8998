sourajitk/msm-4.4

Makefile: Restore GCC 9.3 inline param logic
* This is a follow up commit to 6cbcf0181d09
* Based on the advice by arter97 to restore the inline params
* This overcomes the regressions introduced in GCC 10

Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

Makefile
@@ -631,7 +631,11 @@ endif
 ifdef CONFIG_LTO
 LTO_CFLAGS    := -flto -flto=jobserver -fno-fat-lto-objects \
                  -fuse-linker-plugin -fwhole-program
-KBUILD_CFLAGS += $(LTO_CFLAGS)
+KBUILD_CFLAGS += $(LTO_CFLAGS)  --param=max-inline-insns-auto=1000 \
+				 --param=inline-min-speedup=15 \
+				 --param=max-inline-insns-single=200 \
+				 --param=max-inline-insns-auto=30 \
+				 --param=early-inlining-insns=14
 LTO_LDFLAGS   := $(LTO_CFLAGS) -Wno-lto-type-mismatch -Wno-psabi
 LDFINAL       := $(CONFIG_SHELL) $(srctree)/scripts/gcc-ld $(LTO_LDFLAGS)
 AR            := $(CROSS_COMPILE)gcc-ar

electimon/android_kernel_essential_msm8998

Makefile
@@ -634,7 +634,11 @@ CLANG_FLAGS	+= -no-integrated-as
 CLANG_FLAGS	+= -Werror=unknown-warning-option
 CLANG_FLAGS    += $(call cc-option, -Wno-misleading-indentation)
 CLANG_FLAGS    += $(call cc-option, -Wno-bool-operation)
-KBUILD_CFLAGS	+= $(CLANG_FLAGS)
+KBUILD_CFLAGS	+= $(CLANG_FLAGS) --param=max-inline-insns-auto=1000 \
+				 --param=inline-min-speedup=15 \
+				 --param=max-inline-insns-single=200 \
+				 --param=max-inline-insns-auto=30 \
+				 --param=early-inlining-insns=14
 KBUILD_AFLAGS	+= $(CLANG_FLAGS)
 endif
 